0cbbd4d64e0e59f1da34963a3942bb1590cf2139
Matheus Bazzo <matheus.bazzo@mercadolivre.com>
2022-07-06 15:31:44 -0300

    Correção controller inicial

--- src/main/java/com/meli/desafio_spring/controller/ProductController.java
+++ src/main/java/com/meli/desafio_spring/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.meli.desafio_spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping('/api/v1')
+@RequestMapping("/api/v1")
 public class ProductController {
 
-    @GetMapping('/hello')
+    @GetMapping("/hello")
     public String getHello() {
-        return 'Ola!'
+        return "Ola!";
     }
 }
