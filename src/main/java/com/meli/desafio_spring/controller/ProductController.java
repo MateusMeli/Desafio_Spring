71e390fba1b4a308259429065cadc70836e3e0ce
Mateus de Oliveira Barbosa <mateus.doliveira@mercadolivre.com>
2022-07-06 15:23:47 -0300

    Adding sample route for API

--- src/main/java/com/meli/desafio_spring/controller/ProductController.java
+++ src/main/java/com/meli/desafio_spring/controller/ProductController.java
@@ -1,4 +1,11 @@
 package com.meli.desafio_spring.controller;
 
+@RestController
+@RequestMapping('/api/v1')
 public class ProductController {
+
+    @GetMapping('/hello')
+    public String getHello() {
+        return 'Ola!'
+    }
 }
