2e6313ab6128f93ccc74a3761d9aab8c664e39b9
gucotrim <gustavo.cotrim@mercadolivre.com>
2022-07-07 17:33:24 -0300

    Create JavaDoc to ProductController

--- src/main/java/com/meli/desafio_spring/controller/ProductController.java
+++ src/main/java/com/meli/desafio_spring/controller/ProductController.java
@@ -9,19 +9,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+/**
+ * Products API Controller Class
+ */
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
     @Autowired
     private ProductService service;
 
+    /**
+     * @param productList
+     * @return Response a list of products (data transfer object)
+     * @see <a href="http://localhost:8080/api/vi/insert-products-request">Add Products</a>
+     * Request to create a list of products
+     */
     @PostMapping("/insert-products-request")
     public ResponseEntity<List<ProductDto>> addProducts(@RequestBody List<Product> productList) {
         return new ResponseEntity<>(service.addProducts(productList), HttpStatus.OK);
     }
 
+    /**
+     * Get a list of all products
+     *
+     * @param category     - ?category=category - Get All Products Filtered By Category
+     * @param freeShipping - ?category=categoryName&freeShipping=true - Get All Products Filtered By Category And FreeShipping
+     * @param prestige     - ?freeShipping=true&prestige=**** - Get All Products Filtered By FreeShipping And Prestige
+     * @param order        - ?category=categoryName&freeShipping=true&order=0 - Get All Products Filtered By Category And FreeShipping And Sorted By Name
+     * @return Response a list of products
+     * To add filters use the RequestParam pattern
+     * @see <a href="http://localhost:8080/api/vi/products">Get All Products</a>
+     */
     @GetMapping("/products")
     public ResponseEntity<List<ProductDto>> getAllProducts(
             @RequestParam(required = false) String category,
@@ -29,6 +48,6 @@ public class ProductController {
             @RequestParam(required = false) String prestige,
             @RequestParam(required = false) Integer order) {
 
-        return new ResponseEntity<List<ProductDto>>(service.getAllProducts(category, freeShipping, prestige, order), HttpStatus.OK);
+        return new ResponseEntity<>(service.getAllProducts(category, freeShipping, prestige, order), HttpStatus.OK);
     }
 }
