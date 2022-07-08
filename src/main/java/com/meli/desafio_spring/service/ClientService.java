63ca9023911d75b2e74334979ab5c06f58e5c783
Mateus de Oliveira Barbosa <mateus.doliveira@mercadolivre.com>
2022-07-08 17:50:58 -0300

    Adicionando filtros para cliente

--- src/main/java/com/meli/desafio_spring/service/ClientService.java
+++ src/main/java/com/meli/desafio_spring/service/ClientService.java
@@ -2,6 +2,7 @@ package com.meli.desafio_spring.service;
 
 import com.meli.desafio_spring.dto.ClientDto;
 import com.meli.desafio_spring.model.Client;
+import com.meli.desafio_spring.model.Product;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -9,6 +10,13 @@ import java.util.List;
 public interface ClientService {
     List<ClientDto> addClients(List<Client> listClient) throws FileNotFoundException;
 
-    List<ClientDto> getAllClients() throws FileNotFoundException;
+    List<ClientDto> getAllClients(
+            String name,
+            String state,
+            Long cpf
+    ) throws FileNotFoundException;
+
+    List<Client> sort(List<Client> list, Integer order);
+
 
 }
